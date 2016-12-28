7327ad537fe522ffadcc6cdd71277b30b0c6de47
CyberK <doritang1@naver.com>
2016-12-28 18:23:41 +0900

    sorting function applied
    qsort() is applied but not completed.

--- libtabfile.cpp
+++ libtabfile.cpp
@@ -14,8 +14,13 @@ struct _worditem
 
 static int comparefunc(const void *a, const void *b){
     int x;
+    //a.word가 b.word보다 크면 +, a.word가 b.word보다 작으면 -, a.word가 b.word와 같으면 0을 반환한다
     x = strcmp(((struct _worditem *)a)->word, ((struct _worditem *)b)->word);
+    //만약 반환값이 0이면, 즉 a.word와 b.word가 같으면
+    //그것이 가리키는 definition주소값이 같은지, 즉 같은 definition을 가리키고 있는지 검사한다.
     if(x==0){
+        //definition의 주소가 다르다면 a와 b는 다른 데이터이므로 +나 -를 반환하고
+        //definition의 주소가 같다면 a와 b는 정확히 같은 데이터이므로 0을 반환한다.
         return ((struct _worditem*)a)->definition - ((struct _worditem*)b)->definition;
     }else{
         return x;
@@ -66,7 +71,7 @@ static bool read_tab_file(char *buffer, struct _worditem *worditems[]){
     char *p, *p1, *p2; //p는 현재 읽고 있는 내용, p1은 읽을 위치
     p = buffer;
 
-    int i = 0;
+    int linenum = 0;
 
     struct _worditem worditem;
 
@@ -85,17 +90,14 @@ static bool read_tab_file(char *buffer, struct _worditem *worditems[]){
 
         worditem.word = p;      //결국 p는 '\0'으로 끝나는 word만 남는다.
         worditem.definition = p2;   //p2위치에서 '\0'으로 끝나기까지는 definition이 된다.
-        memcpy(worditems[i],&worditem,sizeof(struct _worditem));
+        memcpy(worditems[linenum],&worditem,sizeof(struct _worditem)); //worditem의 내용을 _worditem의 길이만큼 읽음
 
         //        바로 위 3줄과 같은 내용이다.
         //        worditems[i]->word = p;
         //        worditems[i]->definition = p2;
 
-        printf("worditems[%d].word: %s\n", i, worditems[i]->word);
-        printf("worditems[%d].definition: %s\n", i, worditems[i]->definition);
-        i++;
         p = p1;
-
+        linenum++;
     }
     return true;
 }
@@ -103,23 +105,30 @@ static bool read_tab_file(char *buffer, struct _worditem *worditems[]){
 void convert_tabfile(const char *filename){
     QFile file;
     file.setFileName(filename);
-//    struct _worditem *array[8];
 
-//    for(int i = 0; i<sizeof(array)/sizeof(struct _worditem *); i++){
-//        array[i] = (struct _worditem *)malloc(sizeof(struct _worditem));
-//    }
-    struct _worditem *array;
-    array = (struct _worditem *)malloc(sizeof(struct _worditem)*8);
+    struct _worditem *array[100];
+    for(int i = 0; i<sizeof(array)/sizeof(struct _worditem *); i++){
+        array[i] = (struct _worditem *)malloc(sizeof(struct _worditem));
+        memset(array[i],0,sizeof(struct _worditem));
+    }
 
 
     if(file.open(QIODevice::ReadOnly | QIODevice::Text))
     {
         char *buf = file.readAll().data();
-        read_tab_file(buf, &array);
+        read_tab_file(buf, array);
         file.close();
+
+        qsort(array,100,sizeof(_worditem *),comparefunc);
+        //qsort(array,sizeof(array)/sizeof(_worditem *),sizeof(_worditem *),comparefunc);
+        for(int i=0; i<sizeof(array)/sizeof(struct _worditem *);i++){
+            printf("item[%d].word: %s\n", i, array[i]->word);
+            printf("item[%d].definition: %s\n", i, array[i]->definition);
+        }
+
     }
 
-    for(int i = 0; i<sizeof(&array)/sizeof(struct _worditem *); i++){
-        free(&array[i]);
+    for(int i = 0; i<sizeof(array)/sizeof(struct _worditem *); i++){
+        free(array[i]);
     }
 }
